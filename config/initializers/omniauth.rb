ca21f40477e7c0918813aef32532fe3798c9f0d6
Christopher Giroir <kelsin@valefor.com>
2014-12-02 07:31:31 -0800

    Have omniauth use the Rails logger

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,3 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :bnet, ENV['BNET_KEY'], ENV['BNET_SECRET'], scope: 'wow.profile'
 end
+
+# Use the rails logger
+OmniAuth.config.logger = Rails.logger
