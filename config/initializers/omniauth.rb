ef7ddf9cd2c129e71fe0db741677d2cf0030a662
Christopher Giroir <kelsin@valefor.com>
2014-12-01 20:49:03 -0800

    Adding omniauth config

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,4 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :developer unless Rails.env.production?
+  provider :bnet, ENV['BNET_KEY'], ENV['BNET_SECRET'], scope: 'wow.profile'
+end
