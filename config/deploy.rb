22824db7521bde2e211f862589b067700e45dffd
Christopher Giroir <kelsin@valefor.com>
2014-12-01 23:58:34 -0800

    Adding ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,3 +6,4 @@ set :repo_url, 'git@github.com:kelsin/byfirebepurged.git'
 
 set :ssh_options, keys: ["config/deploy_id_rsa"], forward_agent: true if File.exist?("config/deploy_id_rsa")
 set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
+set :rbenv_ruby, '2.1.3'
