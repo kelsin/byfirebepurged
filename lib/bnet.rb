b37e18e0d8d6fbed77ecf0ad6f29e5fe98b30e79
Christopher Giroir <kelsin@valefor.com>
2014-12-02 21:57:36 -0800

    Adding comments to bnet.rb

--- lib/bnet.rb
+++ lib/bnet.rb
@@ -2,6 +2,7 @@ class Bnet
   include HTTParty
   base_uri 'https://us.api.battle.net'
 
+  # Loads the list of wow characters given a user's oauth access token
   def characters(access_token)
     response = self.class.get('/wow/user/characters',
                               :headers => {
@@ -11,6 +12,7 @@ class Bnet
     response['characters'] || []
   end
 
+  # Loads a character from the wow api
   def character(name, realm)
     self.class.get("/wow/character/#{realm}/#{name}",
                    :query => {
@@ -19,6 +21,7 @@ class Bnet
                    })
   end
 
+  # Loads a character's ilvl from the wow api
   def ilvl(name, realm)
     character(name, realm).try(:[], 'items').try(:[], 'averageItemLevel')
   end
