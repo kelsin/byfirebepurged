4d73a612d223a0cbf029a2a0a65abf151150ed62
Christopher Giroir <kelsin@valefor.com>
2014-12-07 10:58:48 -0800

    Adding support for staging cap deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,3 +3,4 @@ role :web, %w{travis@zealot.kelsin.net}
 role :db,  %w{travis@zealot.kelsin.net}
 
 set :deploy_to, '/home/travis/byfirebepurged-staging'
+set :default_env, { 'DATABASE_URL' => ENV['CAP_STAGING_DATABASE_URL'] }
