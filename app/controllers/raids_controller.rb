a37e749640b6e5b7c49320c279f4ea6d8a552542
Christopher Giroir <kelsin@valefor.com>
2014-12-06 23:15:56 -0800

    Uniq accounts on raids

--- app/controllers/raids_controller.rb
+++ app/controllers/raids_controller.rb
@@ -7,7 +7,7 @@ class RaidsController < ApplicationController
       authorize! :read, raid
     end
 
-    @all_accounts = @raids.map(&:account)
+    @all_accounts = @raids.map(&:account).uniq.sort
     @all_signups = @raids.inject([]) do |signups, raid|
       signups + raid.signups
     end
