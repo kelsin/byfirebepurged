8ac623ef2ed8469d6d3ee0d1c74ab39f3bc0b0e1
Christopher Giroir <kelsin@valefor.com>
2014-12-08 20:23:57 -0800

    Fixing tests to use application/json+ember for now

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -119,6 +119,10 @@ RSpec.configure do |config|
     DatabaseCleaner.clean_with(:truncation)
   end
 
+  config.before(:each) do
+    header 'Accept', 'application/json+ember'
+  end
+
   config.around(:each) do |example|
     DatabaseCleaner.cleaning do
       example.run
