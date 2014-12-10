918a5aa7e8333ef40d90c4bf6689f42944cb8615
Christopher Giroir <kelsin@valefor.com>
2014-12-09 22:44:45 -0800

    Adding tests for /raid/{id}

--- spec/apis/raids_api_spec.rb
+++ spec/apis/raids_api_spec.rb
@@ -5,78 +5,175 @@ RSpec.describe "Raids Api", :type => :api do
     header 'Accept', 'application/json+ember'
   end
 
-  describe '/raids' do
-    describe 'while logged out' do
-      it 'should return a 401' do
+  describe 'while logged out' do
+    it '/raids should return a 401' do
+      get '/raids'
+
+      expect(last_response).to_not be_ok
+      expect(last_response.status).to equal(401)
+    end
+
+    it '/raids/{id} should return a 401' do
+      get '/raids/1'
+
+      expect(last_response).to_not be_ok
+      expect(last_response.status).to equal(401)
+    end
+  end
+
+  describe 'while logged in' do
+    before do
+      login
+    end
+
+    describe 'with no raids' do
+      it '/raids should return an empty result' do
         get '/raids'
+
+        expect(last_response).to be_ok
+        expect(last_response.body).to have_json_size(0).at_path("raids")
+        expect(last_response.body).to have_json_size(0).at_path("signups")
+        expect(last_response.body).to have_json_size(0).at_path("characters")
+        expect(last_response.body).to have_json_size(0).at_path("guilds")
+        expect(last_response.body).to have_json_size(0).at_path("roles")
+      end
+
+      it '/raids/{id} should return a 401' do
+        get '/raids/1'
 
         expect(last_response).to_not be_ok
         expect(last_response.status).to equal(401)
       end
     end
 
-    describe 'while logged in' do
+    describe 'with a no-permission raid' do
       before do
-        login
+        @raid = create :raid
       end
 
-      describe 'with no raids' do
-        it 'should return an empty result' do
-          get '/raids'
+      it '/raids should return an empty result' do
+        get '/raids'
 
-          expect(last_response).to be_ok
-          expect(last_response.body).to have_json_size(0).at_path("raids")
-          expect(last_response.body).to have_json_size(0).at_path("signups")
-          expect(last_response.body).to have_json_size(0).at_path("characters")
-          expect(last_response.body).to have_json_size(0).at_path("guilds")
-          expect(last_response.body).to have_json_size(0).at_path("roles")
-        end
+        expect(last_response).to be_ok
+        expect(last_response.body).to have_json_size(0).at_path("raids")
+        expect(last_response.body).to have_json_size(0).at_path("signups")
+        expect(last_response.body).to have_json_size(0).at_path("characters")
+        expect(last_response.body).to have_json_size(0).at_path("guilds")
+        expect(last_response.body).to have_json_size(0).at_path("roles")
       end
 
-      describe 'with a no-permission raid' do
-        before do
-          @raid = create :raid
-        end
+      it "/raids/{id} should return a 401" do
+        get "/raids/#{@raid.id}"
 
-        it 'should return an empty result' do
-          get '/raids'
+        expect(last_response).to_not be_ok
+        expect(last_response.status).to equal(401)
+      end
+    end
 
-          expect(last_response).to be_ok
-          expect(last_response.body).to have_json_size(0).at_path("raids")
-          expect(last_response.body).to have_json_size(0).at_path("signups")
-          expect(last_response.body).to have_json_size(0).at_path("characters")
-          expect(last_response.body).to have_json_size(0).at_path("guilds")
-          expect(last_response.body).to have_json_size(0).at_path("roles")
-        end
+    describe 'with a wrong-permission raid' do
+      before do
+        @raid = create :raid
+        @raid.permissions << Permission.new(:level => 'member',
+                                            :key => 'Account|99999')
       end
 
-      describe 'with a wrong-permission raid' do
+      it '/raids should return an empty result' do
+        get '/raids'
+
+        expect(last_response).to be_ok
+        expect(last_response.body).to have_json_size(0).at_path("raids")
+        expect(last_response.body).to have_json_size(0).at_path("signups")
+        expect(last_response.body).to have_json_size(0).at_path("characters")
+        expect(last_response.body).to have_json_size(0).at_path("guilds")
+        expect(last_response.body).to have_json_size(0).at_path("roles")
+      end
+
+      it "/raids/{id} should return a 401" do
+        get "/raids/#{@raid.id}"
+
+        expect(last_response).to_not be_ok
+        expect(last_response.status).to equal(401)
+      end
+    end
+
+    describe 'with a account-permission raid' do
+      before do
+        @raid = create :raid
+        @raid.permissions << Permission.new(:level => 'member',
+                                            :key => @account.to_permission)
+      end
+
+      it '/raids should return 1 result' do
+        get '/raids'
+
+        expect(last_response).to be_ok
+        expect(last_response.body).to have_json_size(1).at_path("raids")
+        expect(last_response.body).to have_json_size(0).at_path("signups")
+        expect(last_response.body).to have_json_size(0).at_path("characters")
+        expect(last_response.body).to have_json_size(0).at_path("guilds")
+        expect(last_response.body).to have_json_size(0).at_path("roles")
+      end
+
+      it '/raids/{id} should return a raid' do
+        get "/raids/#{@raid.id}"
+
+        expect(last_response).to be_ok
+        expect(last_response.body).to be_json_eql(@raid.name.to_json).at_path("raid/name")
+        expect(last_response.body).to have_json_size(0).at_path("signups")
+        expect(last_response.body).to have_json_size(0).at_path("characters")
+        expect(last_response.body).to have_json_size(0).at_path("guilds")
+        expect(last_response.body).to have_json_size(0).at_path("roles")
+      end
+    end
+
+    describe 'with a admin-permission raid' do
+      before do
+        @raid = create :raid
+        @raid.permissions << Permission.new(:level => 'admin',
+                                            :key => @account.to_permission)
+      end
+
+      it '/raids should return 1 result' do
+        get '/raids'
+
+        expect(last_response).to be_ok
+        expect(last_response.body).to have_json_size(1).at_path("raids")
+        expect(last_response.body).to have_json_size(0).at_path("signups")
+        expect(last_response.body).to have_json_size(0).at_path("characters")
+        expect(last_response.body).to have_json_size(0).at_path("guilds")
+        expect(last_response.body).to have_json_size(0).at_path("roles")
+      end
+
+      it '/raids/{id} should return a raid' do
+        get "/raids/#{@raid.id}"
+
+        expect(last_response).to be_ok
+        expect(last_response.body).to be_json_eql(@raid.name.to_json).at_path("raid/name")
+        expect(last_response.body).to have_json_size(0).at_path("signups")
+        expect(last_response.body).to have_json_size(0).at_path("characters")
+        expect(last_response.body).to have_json_size(0).at_path("guilds")
+        expect(last_response.body).to have_json_size(0).at_path("roles")
+      end
+    end
+
+    describe 'with 2 characters' do
+      before do
+        @character1 = create(:character,
+                             :account => @account)
+        @guild = @character1.guild
+        @character2 = create(:character,
+                             :guild => @guild,
+                             :account => @account)
+      end
+
+      describe 'with a guild-permission raid' do
         before do
           @raid = create :raid
           @raid.permissions << Permission.new(:level => 'member',
-                                              :key => 'Account|99999')
+                                              :key => @guild.to_permission)
         end
 
-        it 'should return an empty result' do
-          get '/raids'
-
-          expect(last_response).to be_ok
-          expect(last_response.body).to have_json_size(0).at_path("raids")
-          expect(last_response.body).to have_json_size(0).at_path("signups")
-          expect(last_response.body).to have_json_size(0).at_path("characters")
-          expect(last_response.body).to have_json_size(0).at_path("guilds")
-          expect(last_response.body).to have_json_size(0).at_path("roles")
-        end
-      end
-
-      describe 'with a account-permission raid' do
-        before do
-          @raid = create :raid
-          @raid.permissions << Permission.new(:level => 'member',
-                                              :key => @account.to_permission)
-        end
-
-        it 'should return 1 result' do
+        it '/raids should return 1 result' do
           get '/raids'
 
           expect(last_response).to be_ok
@@ -86,16 +183,27 @@ RSpec.describe "Raids Api", :type => :api do
           expect(last_response.body).to have_json_size(0).at_path("guilds")
           expect(last_response.body).to have_json_size(0).at_path("roles")
         end
+
+        it '/raids/{id} should return a raid' do
+          get "/raids/#{@raid.id}"
+
+          expect(last_response).to be_ok
+          expect(last_response.body).to be_json_eql(@raid.name.to_json).at_path("raid/name")
+          expect(last_response.body).to have_json_size(0).at_path("signups")
+          expect(last_response.body).to have_json_size(0).at_path("characters")
+          expect(last_response.body).to have_json_size(0).at_path("guilds")
+          expect(last_response.body).to have_json_size(0).at_path("roles")
+        end
       end
 
-      describe 'with a admin-permission raid' do
+      describe 'with a character-permission raid' do
         before do
           @raid = create :raid
-          @raid.permissions << Permission.new(:level => 'admin',
-                                              :key => @account.to_permission)
+          @raid.permissions << Permission.new(:level => 'member',
+                                              :key => @character1.to_permission)
         end
 
-        it 'should return 1 result' do
+        it '/raids should return 1 result' do
           get '/raids'
 
           expect(last_response).to be_ok
@@ -105,54 +213,16 @@ RSpec.describe "Raids Api", :type => :api do
           expect(last_response.body).to have_json_size(0).at_path("guilds")
           expect(last_response.body).to have_json_size(0).at_path("roles")
         end
-      end
 
-      describe 'with 2 characters' do
-        before do
-          @character1 = create(:character,
-                               :account => @account)
-          @guild = @character1.guild
-          @character2 = create(:character,
-                               :guild => @guild,
-                               :account => @account)
-        end
+        it '/raids/{id} should return a raid' do
+          get "/raids/#{@raid.id}"
 
-        describe 'with a guild-permission raid' do
-          before do
-            @raid = create :raid
-            @raid.permissions << Permission.new(:level => 'member',
-                                                :key => @guild.to_permission)
-          end
-
-          it 'should return 1 result' do
-            get '/raids'
-
-            expect(last_response).to be_ok
-            expect(last_response.body).to have_json_size(1).at_path("raids")
-            expect(last_response.body).to have_json_size(0).at_path("signups")
-            expect(last_response.body).to have_json_size(0).at_path("characters")
-            expect(last_response.body).to have_json_size(0).at_path("guilds")
-            expect(last_response.body).to have_json_size(0).at_path("roles")
-          end
-        end
-
-        describe 'with a character-permission raid' do
-          before do
-            @raid = create :raid
-            @raid.permissions << Permission.new(:level => 'member',
-                                                :key => @character1.to_permission)
-          end
-
-          it 'should return 1 result' do
-            get '/raids'
-
-            expect(last_response).to be_ok
-            expect(last_response.body).to have_json_size(1).at_path("raids")
-            expect(last_response.body).to have_json_size(0).at_path("signups")
-            expect(last_response.body).to have_json_size(0).at_path("characters")
-            expect(last_response.body).to have_json_size(0).at_path("guilds")
-            expect(last_response.body).to have_json_size(0).at_path("roles")
-          end
+          expect(last_response).to be_ok
+          expect(last_response.body).to be_json_eql(@raid.name.to_json).at_path("raid/name")
+          expect(last_response.body).to have_json_size(0).at_path("signups")
+          expect(last_response.body).to have_json_size(0).at_path("characters")
+          expect(last_response.body).to have_json_size(0).at_path("guilds")
+          expect(last_response.body).to have_json_size(0).at_path("roles")
         end
       end
     end
