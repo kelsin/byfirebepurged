cdf4b0f7dd91ceef6000b7cb2e45a948d7862f7b
Christopher Giroir <kelsin@valefor.com>
2014-12-09 19:45:47 -0800

    Making index test a bit more involved

--- spec/apis/index_api_spec.rb
+++ spec/apis/index_api_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "Index Api", :type => :api do
   describe('/') do
-    it('should return env information') do
+    it('should return static API information') do
       get('/')
 
       expect(last_response).to be_ok
-      expect(last_response.body).to have_json_path('env')
-      expect(last_response.body).to have_json_path('api/login')
+      expect(last_response.body).to be_json_eql("test".to_json).at_path('env')
+      expect(last_response.body).to be_json_eql("http://example.org/login".to_json).at_path('api/login')
       expect(last_response.body).to have_json_path('sites/website')
       expect(last_response.body).to have_json_path('sites/docs')
     end
