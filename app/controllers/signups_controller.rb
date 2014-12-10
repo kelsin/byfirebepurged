fd424e9ee0eb0a0440d0de7447b3d173357e06a1
Christopher Giroir <kelsin@valefor.com>
2014-12-09 22:50:57 -0800

    Removing bad attempt at permissions

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -62,20 +62,11 @@ class SignupsController < ApplicationController
   end
 
   def allowed_params
-    if @raid
-      if @account.admin?(@raid)
-        params.require(:signup).permit(:character, :character_id,
-                                       :role, :role_id,
-                                       { :roles => [] }, { :role_ids => [] },
-                                       :note, :preferred, :seated)
-      else
-        params.require(:signup).permit(:character, :character_id,
-                                       { :roles => [] }, { :role_ids => [] },
-                                       :note, :preferred)
-      end
-    else
-      params.require(:signup).permit(:raid, :raid_id)
-    end
+    params.require(:signup).permit(:character, :character_id,
+                                   :raid, :raid_id,
+                                   :role, :role_id,
+                                   { :roles => [] }, { :role_ids => [] },
+                                   :note, :preferred, :seated)
   end
 
   def load_raid
