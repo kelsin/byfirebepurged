47f1ea8de50424f142c040cce6cea6f1a0383aba
Christopher Giroir <kelsin@valefor.com>
2014-12-10 20:15:42 -0800

    Signup creating

--- spec/apis/signups_api_spec.rb
+++ spec/apis/signups_api_spec.rb
@@ -37,9 +37,9 @@ RSpec.describe 'Raids Api', :type => :api do
         expect(last_response.status).to equal(401)
       end
 
-      describe 'with permission' do
+      describe 'with member permission' do
         before do
-          @raid.permissions << Permission.new(:level => 'admin',
+          @raid.permissions << Permission.new(:level => 'member',
                                               :key => @account.to_permission)
         end
 
@@ -52,6 +52,114 @@ RSpec.describe 'Raids Api', :type => :api do
             expect(last_response.body).to have_json_size(0).at_path('characters')
             expect(last_response.body).to have_json_size(0).at_path('guilds')
             expect(last_response.body).to have_json_size(3).at_path('roles')
+          end
+
+          it 'POST /raids/{id}/signups should create a signup' do
+            post "/raids/#{@raid.id}/signups", {
+                   :signup => {
+                     :raid_id => @raid.id,
+                     :character_id => @character1.id,
+                     :note => 'Note',
+                     :role_ids => [@dps.id] }}
+
+            expect(last_response).to be_ok
+            expect(last_response.body).to be_json_eql('Note'.to_json).at_path('signup/note')
+            expect(last_response.body).to be_json_eql([@dps.id].to_json).at_path('signup/roles')
+            expect(last_response.body).to have_json_size(1).at_path('characters')
+            expect(last_response.body).to have_json_size(1).at_path('guilds')
+            expect(last_response.body).to have_json_size(3).at_path('roles')
+          end
+
+          it 'POST /raids/{id}/signups with ember style param names should create a signup' do
+            post "/raids/#{@raid.id}/signups", {
+                   :signup => {
+                     :raid => @raid.id,
+                     :character => @character1.id,
+                     :note => 'Note',
+                     :roles => [@dps.id] }}
+
+            expect(last_response).to be_ok
+            expect(last_response.body).to be_json_eql('Note'.to_json).at_path('signup/note')
+            expect(last_response.body).to be_json_eql([@dps.id].to_json).at_path('signup/roles')
+            expect(last_response.body).to have_json_size(1).at_path('characters')
+            expect(last_response.body).to have_json_size(1).at_path('guilds')
+            expect(last_response.body).to have_json_size(3).at_path('roles')
+          end
+
+          it 'POST /signups with ember style param names should create a signup' do
+            post '/signups', {
+                   :signup => {
+                     :raid => @raid.id,
+                     :character => @character1.id,
+                     :note => 'Note',
+                     :roles => [@dps.id] }}
+
+            expect(last_response).to be_ok
+            expect(last_response.body).to be_json_eql('Note'.to_json).at_path('signup/note')
+            expect(last_response.body).to be_json_eql([@dps.id].to_json).at_path('signup/roles')
+            expect(last_response.body).to have_json_size(1).at_path('characters')
+            expect(last_response.body).to have_json_size(1).at_path('guilds')
+            expect(last_response.body).to have_json_size(3).at_path('roles')
+          end
+
+          it 'POST /raids/{id}/signups with a bad character should fail' do
+            post "/raids/#{@raid.id}/signups", {
+                   :signup => {
+                     :raid_id => @raid.id,
+                     :character_id => 9999,
+                     :note => 'Note',
+                     :role_ids => [@dps.id] }}
+
+            expect(last_response).to_not be_ok
+            expect(last_response.status).to equal(404)
+          end
+
+          it 'POST /signups with a bad character should fail' do
+            post '/signups', {
+                   :signup => {
+                     :raid_id => @raid.id,
+                     :character_id => 9999,
+                     :note => 'Note',
+                     :role_ids => [@dps.id] }}
+
+            expect(last_response).to_not be_ok
+            expect(last_response.status).to equal(404)
+          end
+
+          it 'POST /signups with a bad raid should fail' do
+            post '/signups', {
+                   :signup => {
+                     :raid_id => 9999,
+                     :character_id => @character1.id,
+                     :note => 'Note',
+                     :role_ids => [@dps.id] }}
+
+            expect(last_response).to_not be_ok
+            expect(last_response.status).to equal(404)
+          end
+
+          it 'POST /raids/{id}/signups without a role should fail' do
+            post "/raids/#{@raid.id}/signups", {
+                   :signup => {
+                     :raid_id => @raid.id,
+                     :character_id => @character1.id,
+                     :note => 'Note',
+                     :role_ids => [] }}
+
+            expect(last_response).to_not be_ok
+            expect(last_response.status).to equal(400)
+          end
+
+          it 'POST /signups without a role should fail' do
+            post '/signups', {
+                   :signup => {
+                     :raid_id => @raid.id,
+                     :character_id => @character1.id,
+                     :note => 'Note',
+                     :role_ids => [] }}
+
+            expect(last_response).to_not be_ok
+            expect(last_response.status).to equal(400)
           end
         end
 
