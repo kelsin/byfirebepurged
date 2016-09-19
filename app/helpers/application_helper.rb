25e8bca11c96e66e7ec4b7ee2f5f9c08ec92cbd9
Christopher Giroir <cgiroir@blizzard.com>
2016-09-19 14:56:00 -0700

    No extra whitespace around revision

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def app_revision
     revision_file = Rails.root.join("REVISION")
     if File.readable?(revision_file)
-      IO.read(revision_file)
+      IO.read(revision_file).strip
     else
       "unknown"
     end
